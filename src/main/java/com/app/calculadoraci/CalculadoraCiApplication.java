318fda7c9d5fa892f3426de12ae8bd374c28a41d
karoline <karolinessilva@live.com>
2024-03-19 22:29:36 -0300

    feat: :heavy_plus_sign: Adicionando o projeto maven que realizara a calculadora

---
+++ src/main/java/com/app/calculadoraci/CalculadoraCiApplication.java
@@ -0,0 +1,13 @@
+package com.app.calculadoraci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/calculadoraci/CalculadoraCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.calculadoraci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
